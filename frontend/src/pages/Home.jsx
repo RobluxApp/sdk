61d2c53cc21449670bbdab9d089c76007b6e66a5
Roblux <esmajandax12@gmail.com>
2026-02-20 21:01:34 +0700

    Create Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Roblux</h1>
+      <p>Mint your collectibles, trade or flip them, and redeem whenever you want.</p>
+    </div>
+  );
+}
+
+export default Home;
